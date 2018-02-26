a29bc7fbe68c54423fe1effa504e0a4a2f45ac12
patrickatcw <pj8392@yahoo.com>
2018-02-26 10:32:28 -0500

    creating fields in super class Animal

--- src/com/me/Animal.java
+++ src/com/me/Animal.java
@@ -1,8 +1,13 @@
 package com.me;
 
 //step 1 created Animal class
-public class Animal {
+public class Animal {           //Animal is super class
 
-
+        //step 4 creating fields in super class
+        private String name;
+        private int brain;
+        private int body;
+        private int size;
+        private int weight;
 
 }
