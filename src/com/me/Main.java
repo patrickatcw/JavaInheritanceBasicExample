c1e85004da4609866e633803b67caee4a6ab5b33
patrickatcw <pj8392@yahoo.com>
2018-02-26 10:58:22 -0500

    created instances in main and checking eat method from Animal super class

--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -1,4 +1,18 @@
 package com.me;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        //step 11 creating Animal instance
+        Animal animal = new Animal("Animal", 1,1, 5, 5);
+
+        //step 12 creating Dog instance
+        Dog dog = new Dog("Yorkie", 8, 20, 2, 4, 1, 20, "Long silky");
+
+        //step 13 using methods from animal class
+        dog.eat();
+
+    }
+
 }
